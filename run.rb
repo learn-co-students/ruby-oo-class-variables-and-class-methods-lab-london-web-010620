8cda54fc1af526cba44e18eac33644a87d20215a
santiago molinari <santomolinari@gmail.com>
2020-01-10 12:25:16 +0000

    Done.

---
+++ run.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/song'
+require 'pry'
+
+forever_young = Song.new(forever_young, "Alphaville", "Glamm")
+
+everywhere = Song.new(everywhere, "Fleetwood Mac", "Great Rock")
+
+
+binding.pry
